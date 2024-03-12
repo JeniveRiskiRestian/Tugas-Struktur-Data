1aecfc7394451989bbc0de72d7c4461c71f18f92
Jenyve Riski Restian <162582739+JeniveRiskiRestian@users.noreply.github.com>
2024-03-12 08:04:11 +0700

    Add files via upload

---
+++ DasarJava/src/Perulangan/LoopingWhile.java
@@ -0,0 +1,13 @@
+package Perulangan;
+
+public class LoopingWhile {
+    public static void main(String[] args) {
+        int j = 1;
+        while (j <= 5){
+            System.out.print(j+" ");
+            j++;
+        }
+        System.out.println("");
+    }
+    
+}
