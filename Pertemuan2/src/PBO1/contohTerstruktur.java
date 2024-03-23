fc29f2c25866660f17b76b40ba45fecc725eadd6
Jenyve Riski Restian <162582739+JeniveRiskiRestian@users.noreply.github.com>
2024-03-23 20:36:46 +0700

    Add files via upload

---
+++ Pertemuan2/src/PBO1/contohTerstruktur.java
@@ -0,0 +1,13 @@
+package PBO1;
+public class contohTerstruktur {
+
+    public static void main(String[] args) {
+      double phi = 3.14;
+      int jari2 = 10;
+      double luas = 0;
+      luas = phi * (jari2*jari2);
+      
+        System.out.println("Luas Lingkaran = " + luas);
+    }
+    
+}
